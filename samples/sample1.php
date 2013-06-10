699daa47fd553736ef47985db0fd132097d1b583
Jan Eichhorn <jan@pixel-web.org>
2013-06-10 19:34:03 +0200

    added phpunit bootstrap

--- samples/sample1.php
+++ samples/sample1.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'autoloader.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'bootstrap.php';
 require_once 'Config.php';
 
 use ApaiIO\Request\RequestFactory;
