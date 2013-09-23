fb2b13daba332414861fac27f9075c0b1063e72c
Exeu <exeu65@googlemail.com>
2013-09-23 21:51:27 +0200

    removed universalclassloader dependency added composer to build xml added composer to travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,10 +15,9 @@
  * limitations under the License.
  */
 
-require_once 'UniversalClassLoader.php';
-
-$classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-
-$classLoader->registerNamespace('ApaiIO\Test', __DIR__);
-$classLoader->registerNamespace('ApaiIO', __DIR__.'/../lib');
-$classLoader->register();
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
+$loader->add('ApaiIO\Test', __DIR__);
