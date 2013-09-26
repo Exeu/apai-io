dcd2ef55787bd82f0337dd82fef2f2e53a308a0b
Exeu <exeu65@googlemail.com>
2013-09-26 19:34:16 +0200

    moved concrete request tests in a subfolder

--- tests/ApaiIO/Test/Request/Types/RestRequestTest.php
+++ tests/ApaiIO/Test/Request/Types/RestRequestTest.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-namespace ApaiIO\Test\Request;
+namespace ApaiIO\Test\Request\Types;
 
 use ApaiIO\Configuration\GenericConfiguration;
 use ApaiIO\ApaiIO;
