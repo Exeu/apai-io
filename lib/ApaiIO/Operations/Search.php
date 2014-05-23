2962a98a8a17153a631fbfca9f2c42ad0f67d795
Napolux <napolux@gmail.com>
2014-05-23 20:06:33 +0200

    setMaximumPrice/setMinimumPrice

--- lib/ApaiIO/Operations/Search.php
+++ lib/ApaiIO/Operations/Search.php
@@ -84,4 +84,56 @@ class Search extends AbstractOperation
 
         return $this;
     }
+
+    /**
+     * Sets the minimum price to a specified value for the search
+     * Currency will be given by the site you are querying: EUR for IT, USD for COM
+     * Price should be given as integer. 8.99$ USD becomes 899
+     * 
+     *
+     * @param integer $price
+     *
+     * @return \ApaiIO\Operations\Search
+     */
+    public function setMinimumPrice($price)
+    {
+        if (false === is_numeric($price) || $price < 1) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '%s is an invalid minimum price value. It has to be numeric and >= than 1',
+                    $price
+                )
+            );
+        }
+
+        $this->parameter['MinimumPrice'] = $price;
+
+        return $this;
+    }
+    
+    /**
+     * Sets the maximum price to a specified value for the search
+     * Currency will be given by the site you are querying: EUR for IT, USD for COM
+     * Price should be given as integer. 8.99$ USD becomes 899
+     * 
+     *
+     * @param integer $price
+     *
+     * @return \ApaiIO\Operations\Search
+     */    
+    public function setMaximumPrice($price)
+    {
+        if (false === is_numeric($price)  || $price < 1) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '%s is an invalid maximum price value. It has to be numeric and >= than 1',
+                    $price
+                )
+            );
+        }
+
+        $this->parameter['MaximumPrice'] = $price;
+
+        return $this;
+    }
 }
