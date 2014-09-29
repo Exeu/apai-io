3315536710f7e59d80eb24cfe3eceb4542d78f83
Exeu <exeu65@googlemail.com>
2014-09-29 11:23:40 +0200

    refactored the samples

---
+++ samples/BrowseNodeLookup/SimpleBrowseNodeLookup.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright 2013 Jan Eichhorn <exeu65@googlemail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'bootstrap.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Config.php';
+
+use ApaiIO\ApaiIO;
+use ApaiIO\Configuration\GenericConfiguration;
+use ApaiIO\Operations\BrowseNodeLookup;
+
+$conf = new GenericConfiguration();
+
+try {
+    $conf
+        ->setCountry('de')
+        ->setAccessKey(AWS_API_KEY)
+        ->setSecretKey(AWS_API_SECRET_KEY)
+        ->setAssociateTag(AWS_ASSOCIATE_TAG);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+$apaiIO = new ApaiIO($conf);
+
+// Changing to SOAP and ObjectToArray ResponseTransformer
+$conf->setRequest('\ApaiIO\Request\Soap\Request');
+$conf->setResponseTransformer(new \ApaiIO\ResponseTransformer\ObjectToArray());
+
+$browseNodeLookup = new BrowseNodeLookup();
+$browseNodeLookup->setNodeId(542064);
+
+$formattedResponse = $apaiIO->runOperation($browseNodeLookup);
+
+var_dump($formattedResponse);
